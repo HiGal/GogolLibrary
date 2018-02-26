fd49416d77e8361e9f6a647c8ca39d994c7c96d0
HiGal <f.galeev@innopolis.ru>
2018-02-26 19:17:04 +0300

    Initial commit

---
+++ src/main/java/com/project/glib/GlibApplication.java
@@ -0,0 +1,12 @@
+package com.project.glib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlibApplication {
+
+	public static void main(String[] args) {
+		 SpringApplication.run(GlibApplication.class, args);
+	}
+}
---
+++ src/main/java/com/project/glib/loginController.java
@@ -0,0 +1,15 @@
+package com.project.glib;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class loginController {
+
+    @GetMapping("/login")
+    public ModelAndView login(){
+        return new ModelAndView("login");
+    }
+
+}
