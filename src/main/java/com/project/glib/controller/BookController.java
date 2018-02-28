9c2a2efa9d6a6e729a2a58eefe853f758a32ce3d
imazan00 <i.mazan@innopolis.ru>
2018-02-28 21:02:11 +0300

    Add smth

--- src/main/java/com/project/glib/controller/BookController.java
+++ src/main/java/com/project/glib/controller/BookController.java
@@ -20,7 +20,7 @@ public class BookController {
     }
 
     @RequestMapping(value = "books", method = RequestMethod.GET)
-    public String listBooks(Model model){
+    public String listBooks(Model model) {
         model.addAttribute("book", new Book());
         model.addAttribute("listBooks", this.bookService.listBooks());
 
@@ -28,10 +28,10 @@ public class BookController {
     }
 
     @RequestMapping(value = "/books/add", method = RequestMethod.POST)
-    public String addBook(@ModelAttribute("book") Book book){
-        if(book.getId() == 0){
+    public String addBook(@ModelAttribute("book") Book book) {
+        if (book.getId() == 0) {
             this.bookService.addBook(book);
-        }else {
+        } else {
             this.bookService.updateBook(book);
         }
 
@@ -39,14 +39,14 @@ public class BookController {
     }
 
     @RequestMapping("/remove/{id}")
-    public String removeBook(@PathVariable("id") int id){
+    public String removeBook(@PathVariable("id") int id) {
         this.bookService.removeBook(id);
 
         return "redirect:/books";
     }
 
     @RequestMapping("edit/{id}")
-    public String editBook(@PathVariable("id") int id, Model model){
+    public String editBook(@PathVariable("id") int id, Model model) {
         model.addAttribute("book", this.bookService.getBookById(id));
         model.addAttribute("listBooks", this.bookService.listBooks());
 
@@ -54,7 +54,7 @@ public class BookController {
     }
 
     @RequestMapping("bookdata/{id}")
-    public String bookData(@PathVariable("id") int id, Model model){
+    public String bookData(@PathVariable("id") int id, Model model) {
         model.addAttribute("book", this.bookService.getBookById(id));
 
         return "bookdata";
---
+++ src/main/java/com/project/glib/model/AudioVideo.java
@@ -0,0 +1,71 @@
+package com.project.glib.model;
+
+import javax.persistence.*;
+
+@Entity
+public class AudioVideo extends Document {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    private String avTitle;
+
+    @Column(name = "author")
+    private String avAuthor;
+
+    @Column(name = "price")
+    private int price;
+
+    protected AudioVideo() {
+    }
+
+    public AudioVideo(String avTitle, String avAuthor, int price) {
+        this.avTitle = avTitle;
+        this.avAuthor = avAuthor;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "AudioVideo{" +
+                "id=" + id +
+                ", avTitle='" + avTitle + '\'' +
+                ", avAuthor='" + avAuthor + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAvTitle() {
+        return avTitle;
+    }
+
+    public void setAvTitle(String avTitle) {
+        this.avTitle = avTitle;
+    }
+
+    public String getAvAuthor() {
+        return avAuthor;
+    }
+
+    public void setAvAuthor(String avAuthor) {
+        this.avAuthor = avAuthor;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
