561403524779c6a8dcdcbc38f97bddc86875385b
HiGal <f.galeev@innopolis.ru>
2018-03-04 20:31:37 +0300

    Fully make front-end

--- src/main/java/com/project/glib/controller/UserController.java
+++ src/main/java/com/project/glib/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class UserController {
     private final UsersDaoImplementation usersDao;
     private final SecurityDaoImplementation securityDao;
