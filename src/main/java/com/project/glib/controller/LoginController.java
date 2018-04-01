07ba4b91471a2e9bb66f26ecbd559ff03eef75c1
HiGal <f.galeev@innopolis.ru>
2018-04-01 16:59:14 +0300

    Connected back-end login with front. And working Dashboard

--- src/main/java/com/project/glib/controller/LoginController.java
+++ src/main/java/com/project/glib/controller/LoginController.java
@@ -4,38 +4,64 @@ import com.project.glib.model.User;
 import com.project.glib.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
 @Controller
-public class LoginController{
+public class LoginController {
 
     @Autowired
     UserService userService;
 
+    public static final String role1 = "LIBRARIAN";
+    public static final String role2 = "STUDENT";
+    public static final String role3 = "TEACHER_ASSISTANT";
+    public static final String role4 = "VISITING_PROFESSOR";
+    public static final String role5 = "PROFESSOR";
+    public static final String role6 = "INSTRUCTOR";
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(){
-        return "login";
+    public ModelAndView login() {
+
+        return new ModelAndView("login");
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(@RequestBody User form){
+    public ModelAndView login( @ModelAttribute User form) {
         System.out.println();
         System.out.println();
         System.out.println(form);
         System.out.println();
         System.out.println();
+        ModelAndView model= new ModelAndView();
         try {
             System.out.println(form.getLogin());
             User user = userService.findByLogin(form.getLogin());
             System.out.println(user);
-            if(user !=null && user.getPassword().equals(form.getPassword()))
-                return "success";
+
+            if (user != null && user.getPassword().equals(form.getPassword())) {
+                model.addObject("info",user);
+                if (user.getRole().equals(role1)) {
+                    System.out.println("librarian");
+                    model.setViewName("librarian");
+                } else if(user.getRole().equals(role2) || user.getRole().equals(role3) ||
+                          user.getRole().equals(role4) || user.getRole().equals(role5)
+                          || user.getRole().equals(role6)){
+                    System.out.println("student");
+                    model.setViewName("student");
+                }
+                System.out.println("return  1");
+                return model;
+            }
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "error";
+        System.out.println("return 2");
+        return model;
     }
 
 }
