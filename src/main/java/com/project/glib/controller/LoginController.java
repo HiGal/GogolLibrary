5198e3064dc0296ae84f267c8a371361f366f203
imazan00 <i.mazan@innopolis.ru>
2018-04-01 17:11:20 +0300

    Modify LoginController

--- src/main/java/com/project/glib/controller/LoginController.java
+++ src/main/java/com/project/glib/controller/LoginController.java
@@ -4,63 +4,52 @@ import com.project.glib.model.User;
 import com.project.glib.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
+
+import java.util.Arrays;
 
 @Controller
 public class LoginController {
+    private final UserService userService;
 
     @Autowired
-    UserService userService;
-
-    public static final String role1 = "LIBRARIAN";
-    public static final String role2 = "STUDENT";
-    public static final String role3 = "TEACHER_ASSISTANT";
-    public static final String role4 = "VISITING_PROFESSOR";
-    public static final String role5 = "PROFESSOR";
-    public static final String role6 = "INSTRUCTOR";
+    public LoginController(UserService userService) {
+        this.userService = userService;
+    }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView login() {
-
         return new ModelAndView("login");
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ModelAndView login( @ModelAttribute User form) {
-        System.out.println();
-        System.out.println();
-        System.out.println(form);
-        System.out.println();
-        System.out.println();
-        ModelAndView model= new ModelAndView();
+    public ModelAndView login(@ModelAttribute User form) {
+        ModelAndView model = new ModelAndView();
         try {
-            System.out.println(form.getLogin());
             User user = userService.findByLogin(form.getLogin());
-            System.out.println(user);
+            String role = user.getRole();
 
-            if (user != null && user.getPassword().equals(form.getPassword())) {
-                model.addObject("info",user);
-                if (user.getRole().equals(role1)) {
-                    System.out.println("librarian");
+            if (user.getPassword().equals(form.getPassword())) {
+                model.addObject("info", user);
+
+                if (role.equals(User.LIBRARIAN)) {
                     model.setViewName("librarian");
-                } else if(user.getRole().equals(role2) || user.getRole().equals(role3) ||
-                          user.getRole().equals(role4) || user.getRole().equals(role5)
-                          || user.getRole().equals(role6)){
-                    System.out.println("student");
+                } else if (role.equals(User.STUDENT)) {
                     model.setViewName("student");
+                } else if (Arrays.asList(User.FACULTY).contains(role)) {
+                    model.setViewName("faculty");
+                } else {
+                    throw new Exception("WRONG ROLE");
                 }
-                System.out.println("return  1");
+
                 return model;
             }
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("return 2");
         return model;
     }
 
