92632f0059b8eb14d3296b3d36a857e4ec2d7c4f
HiGal <f.galeev@innopolis.ru>
2018-04-02 01:45:00 +0300

    Some tryings to save session

--- src/main/java/com/project/glib/controller/LoginController.java
+++ src/main/java/com/project/glib/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.project.glib.controller;
 
 import com.project.glib.model.User;
+import com.project.glib.service.BookService;
 import com.project.glib.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
