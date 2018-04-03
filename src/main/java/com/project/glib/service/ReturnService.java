d4ca53461485e848b9cd5215ceae7986ce02abb4
imazan00 <i.mazan@innopolis.ru>
2018-04-03 19:13:09 +0300

    Fix overdue

--- src/main/java/com/project/glib/service/ReturnService.java
+++ src/main/java/com/project/glib/service/ReturnService.java
@@ -181,6 +181,6 @@ public class ReturnService {
      * @return integers days in nanoseconds
      */
     private int convertToDays(long nanoseconds) {
-        return (int) nanoseconds / 1000 / 1000 / 1000 / 60 / 60 / 24;
+        return (int) Math.abs(nanoseconds) / 1000 / 1000 / 1000 / 60 / 60 / 24;
     }
 }
