86f99f3cb9083153594967397914fe410ef84049
Alexey <alekseyzhuchkov.tut@gmail.com>
2018-04-16 00:11:56 +0300

    Logger system + DB changes

--- src/main/java/com/project/glib/service/LoggerService.java
+++ src/main/java/com/project/glib/service/LoggerService.java
@@ -1,27 +1,38 @@
 package com.project.glib.service;
 
 import com.project.glib.dao.implementations.LoggerDaoImplementation;
-import com.project.glib.model.Logger;
+import com.project.glib.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class LoggerService implements LoggerActions {
     private final LoggerDaoImplementation loggerDao;
+    private final UserService userService;
+    private final DocumentPhysicalService documentPhysicalService;
+    private final BookService bookService;
+    private final JournalService journalService;
+    private final AudioVideoService audioVideoService;
 
     @Autowired
-    public LoggerService(LoggerDaoImplementation loggerDao) {
+    public LoggerService(LoggerDaoImplementation loggerDao, UserService userService, DocumentPhysicalService documentPhysicalService, BookService bookService, JournalService journalService, AudioVideoService audioVideoService) {
         this.loggerDao = loggerDao;
+        this.userService = userService;
+        this.documentPhysicalService = documentPhysicalService;
+        this.bookService = bookService;
+        this.journalService = journalService;
+        this.audioVideoService = audioVideoService;
     }
 
     public void add(Logger logger) throws Exception {
         loggerDao.add(logger);
     }
 
-    public void addLog(long id_user, long virt_doc_id, long phys_doc_id, String action, long date) throws Exception {
-        Logger logger = new Logger(id_user, virt_doc_id, phys_doc_id, action, date);
+    public void addLog(long id_user, long phys_doc_id, String action, long date) throws Exception {
+        Logger logger = new Logger(id_user, phys_doc_id, action, date);
         loggerDao.add(logger);
     }
 
@@ -36,5 +47,42 @@ public class LoggerService implements LoggerActions {
         }
 
         return loggerList;
+    }
+
+    public List<String> showLoggerForaWeekString() throws Exception {
+        List<Logger> loggerList = showLoggerForaWeek();
+        ArrayList<String> stringList = new ArrayList<>();
+        for (Logger aLoggerList : loggerList) {
+            String s = "";
+            s += aLoggerList.getDate();
+
+            // TODO how to check for null this part ->
+            if (aLoggerList.getUserId() > 0) {
+                User user = userService.getById(aLoggerList.getUserId());
+                s += " " + user.getRole() + " " + user.getSurname() + " " + user.getName();
+            }
+
+            s += " " + aLoggerList.getAction();
+
+            // TODO how to check for null this part ->
+            if (aLoggerList.getPhysDocId() > 0) {
+                String type = documentPhysicalService.getTypeById(aLoggerList.getPhysDocId());
+                long id = documentPhysicalService.getDocVirIdById(aLoggerList.getPhysDocId());
+                switch (type) {
+                    case Document.BOOK:
+                        Book book = bookService.getById(id);
+                        s += " " + book.getTitle() + " " + book.getAuthor();
+                    case Document.JOURNAL:
+                        Journal journal = journalService.getById(id);
+                        s += " " + journal.getTitle() + " " + journal.getAuthor();
+                    case Document.AV:
+                        AudioVideo audioVideo = audioVideoService.getById(id);
+                        s += " " + audioVideo.getTitle() + " " + audioVideo.getAuthor();
+                }
+            }
+
+            stringList.add(s);
+        }
+        return stringList;
     }
 }
