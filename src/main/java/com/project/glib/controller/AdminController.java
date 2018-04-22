043334fa6b21b355e3be732dcb2591bd328d73c7
Farit Galeev <f.galeev@innopolis.ru>
2018-04-22 10:53:45 +0300

    Added functional admin pages

--- src/main/java/com/project/glib/controller/AdminController.java
+++ src/main/java/com/project/glib/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.project.glib.controller;
 
 import com.project.glib.model.User;
+import com.project.glib.service.LoggerService;
 import com.project.glib.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,10 +13,12 @@ import javax.servlet.http.HttpServletRequest;
 public class AdminController {
 
     private final UserService userService;
+    private final LoggerService loggerService;
 
     @Autowired
-    public AdminController(UserService userService) {
+    public AdminController(UserService userService, LoggerService loggerService) {
         this.userService = userService;
+        this.loggerService = loggerService;
     }
 
     @RequestMapping(value = "/admin")
@@ -42,6 +45,19 @@ public class AdminController {
             e.printStackTrace();
         }
         return "succ";
+    }
+
+    @RequestMapping(value = "/log")
+    public ModelAndView log(HttpServletRequest request){
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            modelAndView.addObject("actions",loggerService.showLoggerForaWeekString());
+            System.out.println(loggerService.showLoggerForaWeekString());
+            modelAndView.setViewName("log");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return modelAndView;
     }
 
 
