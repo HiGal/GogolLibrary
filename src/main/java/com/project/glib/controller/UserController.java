3d70e326d2cf7e8f2c52c818bca86e2152422b27
Farit Galeev <f.galeev@innopolis.ru>
2018-04-23 11:48:18 +0300

    add default order

--- src/main/java/com/project/glib/controller/UserController.java
+++ src/main/java/com/project/glib/controller/UserController.java
@@ -2,16 +2,15 @@ package com.project.glib.controller;
 
 //import com.project.glib.dao.implementations.SecurityDaoImplementation;
 
-import com.project.glib.model.AudioVideo;
-import com.project.glib.model.Book;
-import com.project.glib.model.Journal;
-import com.project.glib.model.User;
+import com.project.glib.model.*;
+import com.project.glib.service.BookingService;
+import com.project.glib.service.DocumentPhysicalService;
 import com.project.glib.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,10 +18,14 @@ import javax.servlet.http.HttpServletRequest;
 public class UserController {
 
     private final MessageService messageService;
+    private final BookingService bookingService;
+    private final DocumentPhysicalService physicalService;
 
     @Autowired
-    public UserController(MessageService messageService) {
+    public UserController(MessageService messageService, BookingService bookService, DocumentPhysicalService physicalService) {
         this.messageService = messageService;
+        this.bookingService = bookService;
+        this.physicalService = physicalService;
     }
 
     @RequestMapping(value = "/patron")
@@ -35,21 +38,46 @@ public class UserController {
         return modelAndView;
     }
 
-    @RequestMapping(value = "/order/book")
-    public ModelAndView orderBook(@ModelAttribute Book book) {
-        ModelAndView modelAndView = new ModelAndView();
+    @RequestMapping(value = "/order/book", method = RequestMethod.POST)
+    public @ResponseBody
+    ModelAndView orderBook(@RequestBody Book book, HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView(new MappingJackson2JsonView());
+        User user = (User) request.getSession().getAttribute("user");
+        try {
+            bookingService.toBookDocument(book.getId(), Document.BOOK, user.getId());
+            modelAndView.addObject("data","Success! Wait confirmation");
+        } catch (Exception e) {
+            modelAndView.addObject("data",e.getMessage());
+            e.printStackTrace();
+        }
         return modelAndView;
     }
 
     @RequestMapping(value = "/order/journal")
-    public ModelAndView orderBook(@ModelAttribute Journal journal) {
-        ModelAndView modelAndView = new ModelAndView();
+    public @ResponseBody ModelAndView orderBook(@RequestBody Journal journal, HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView(new MappingJackson2JsonView());
+        User user = (User) request.getSession().getAttribute("user");
+        try {
+            bookingService.toBookDocument(journal.getId(), Document.JOURNAL, user.getId());
+            modelAndView.addObject("data","Success! Wait confirmation");
+        } catch (Exception e) {
+            modelAndView.addObject("data",e.getMessage());
+            e.printStackTrace();
+        }
         return modelAndView;
     }
 
-    @RequestMapping(value = "/order/AV")
-    public ModelAndView orderBook(@ModelAttribute AudioVideo audioVideo) {
-        ModelAndView modelAndView = new ModelAndView();
+    @RequestMapping(value = "/order/av")
+    public @ResponseBody ModelAndView orderBook(@RequestBody AudioVideo audioVideo, HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView(new MappingJackson2JsonView());
+        User user = (User) request.getSession().getAttribute("user");
+        try {
+            bookingService.toBookDocument(audioVideo.getId(), Document.AV, user.getId());
+            modelAndView.addObject("data","Success! Wait confirmation");
+        } catch (Exception e) {
+            modelAndView.addObject("data",e.getMessage());
+            e.printStackTrace();
+        }
         return modelAndView;
     }
 
