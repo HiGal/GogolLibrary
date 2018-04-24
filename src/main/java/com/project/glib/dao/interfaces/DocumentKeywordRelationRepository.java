6cf8457402166ecb075a021ea715f980ea2411d5
HiGal <f.galeev@innopolis.ru>
2018-04-24 22:44:50 +0300

    cleaned up

--- src/main/java/com/project/glib/dao/interfaces/DocumentKeywordRelationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.glib.dao.interfaces;
-
-import com.project.glib.model.DocumentKeywordRelation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface DocumentKeywordRelationRepository extends JpaRepository<DocumentKeywordRelation, Long> {
-}
--- src/main/java/com/project/glib/dao/interfaces/DocumentsKeywordsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.glib.dao.interfaces;
-
-import com.project.glib.model.DocumentsKeywords;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource()
-public interface DocumentsKeywordsRepository extends JpaRepository<DocumentsKeywords, Long> {
-}
